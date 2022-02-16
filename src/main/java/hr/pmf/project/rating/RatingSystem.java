b59fed1f12c02cc310247d3dfbb7d3fcf1de8a29
jurica <jura959@gmail.com>
2022-02-16 18:21:06 +0100

    dodani tenisaci

--- src/main/java/hr/pmf/project/rating/RatingSystem.java
+++ src/main/java/hr/pmf/project/rating/RatingSystem.java
@@ -6,8 +6,12 @@ package hr.pmf.project.rating;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 import org.javatuples.Pair;
@@ -34,6 +38,31 @@ public class RatingSystem {
         }
         return ret;
     }
+    
+    /*
+    public static void izdvojiLjude() throws FileNotFoundException, IOException{
+        File file = new File("tenis.txt");
+        Scanner sc = new Scanner(file);
+        HashSet<String> ljudi = new HashSet<>();
+        while(sc.hasNextLine()){
+            String tmp[] = sc.nextLine().split(" ");
+            if(tmp[0].equals("_contest:"))
+                continue;
+            ljudi.add(separiraj(tmp[0]));
+        }
+        Iterator it = ljudi.iterator();
+        String svi = "";
+        while(it.hasNext()){
+            String cur = it.next().toString();
+            System.out.println(cur);
+            svi += cur + "\n";
+        }
+        sc.close();
+        FileWriter fw = new FileWriter("tenisaci.txt");
+        fw.write(svi);
+        fw.close();
+    }
+    */
     
     public List<Event> readEvent(File file) throws SQLException{
         //ucita evente, kalkulira dogadaje i mijenja rejtinge u bazi
@@ -67,10 +96,10 @@ public class RatingSystem {
                     curEvent = new Event(new ArrayList<>());
                 }else{
                     int ranked = Integer.parseInt(tmp[1]);
-                    String playerId = tmp[0];
                     if(eventType == 1){
                         tmp[0] = separiraj(tmp[0]);
                     }
+                    String playerId = tmp[0];
                     nPlayers++;
                     Player curPlayer = null;
                     for(Player p : sviIgraci){
@@ -82,6 +111,7 @@ public class RatingSystem {
                     if(curPlayer != null)
                         curEvent.addPlayer(curPlayer, ranked);
                     else{
+                        //System.out.println("problem je sa" + playerId);
                         throw new SQLException("igrac nije u bazi");
                     }
                 }
@@ -94,11 +124,23 @@ public class RatingSystem {
         return ret;
     }
     
-    public static void main(String args[]) throws SQLException{
+    public static void main(String args[]) throws SQLException, ClassNotFoundException, IOException{
         //ovo ne radi dok ne stavimo ljude u bazu (putem guia il rucno)
-        File file = new File("eventi2.txt");
+        
+        File file = new File("tenis.txt");
         if(file != null){
-            List<Event> tmp = new RatingSystem(0).readEvent(file);
+            List<Event> tmp = new RatingSystem(1).readEvent(file);
         }
+        /*
+        JavaSqlite baza = new JavaSqlite();
+        List<Player> tmp = baza.SelectPlayers("SELECT * FROM Player WHERE player_id > 0");
+        if(tmp != null){
+            for(int i = 0; i < (int)tmp.size(); i++){
+                baza.DeletePlayer(tmp.get(i));
+            }
+        }
+        */
+        
+        //izdvojiLjude();
     }
 }
